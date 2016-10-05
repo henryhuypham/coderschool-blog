957913ff82b6f823e96072d18dd137d909c4c782
Huy Pham <huyphamthanh8290@gmail.com>
2016-10-06 02:14:47 +0700

    Route root to article list

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
     post 'search', on: :collection
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'articles#index'
 end
